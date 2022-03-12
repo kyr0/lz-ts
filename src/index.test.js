aa41ac6549c48c3c6e931315b32d6a3d67d40ebf
Michael Mannseicher <m.mansi1@gmx.net>
2022-03-12 02:22:00 +0100

    build: add ci/cd fix: delete file and add to gitignore

--- src/index.test.js
+++
@@ -1,8 +0,0 @@
-const lz = require('../index')
-
-it('compresses UTF16', () => {
-  const input = 'inputLala123'
-  const compressed = lz.compress(input)
-  const decompressd = lz.decompress(compressed)
-  expect(decompressd).toBe(input)
-})
